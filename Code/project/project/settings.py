0be2b50479c3d9e341085b277980f69f95acdb2d
rhtshahi <iamshahirohit@gmail.com>
2024-02-02 15:01:21 +0545

    2: Django channels and routing

--- Code/project/project/settings.py
+++ Code/project/project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from . import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
